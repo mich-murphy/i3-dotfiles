27206783d75532aeccf09c495004e620cecc9763
hackenbacker - arch <mich@elmurphy.com>
2022-03-17 16:01:37 +1100

    minor change to i3 keybindings

--- install.sh
+++ install.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# install neovim plugin manager
-[ ! -f "$DOTFILES/nvim/autoload/plug.vim" ] \
-    && curl -fLo "$DOTFILES/nvim/autoload/plug.vim" --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-mkdir -p "$XDG_CONFIG_HOME/nvim/autoload"
-ln -sf "$DOTFILES/nvim/autoload/plug.vim" "$XDG_CONFIG_HOME/nvim/autoload/plug.vim"
-
-# Install (or update) all the plugins
-nvim --noplugin +PlugUpdate +qa
-
 # tmux plugin manager
 [ ! -d "$XDG_CONFIG_HOME/tmux/plugins/tpm" ] \
 && git clone https://github.com/tmux-plugins/tpm \
